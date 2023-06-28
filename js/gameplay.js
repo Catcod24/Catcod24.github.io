792688ec44f72567da1241ce8b6199a3d5008adf
Catcod24 <131913682+Catcod24@users.noreply.github.com>
2023-06-28 19:08:07 +0300

    Update gameplay.js

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,1 +1,20 @@
 document.body.style.backgroundImage = "url('img/bg/spatiu.jpg')";
+/*const para = document.createElement("p");
+para.innerText = "This is a paragraph";
+document.body.appendChild(para);
+
+Try it Yourself »
+*/
+var playdiv = document.createElement('div');
+playdiv.id = 'divplay';
+document.body.appendChild(playdiv);
+var playbtn = document.createElement('button');
+playbtn.id = 'btnplay';
+playbtn.addEventListener("click", play);
+playdiv.appendChild(playbtn);
+function play(){
+  playbtn.remove();
+  playdiv.remove();
+}
+
+
