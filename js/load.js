b74395340afb351f519bf3ca36ea44fc68848a1b
Catcod24 <131913682+Catcod24@users.noreply.github.com>
2023-06-30 15:06:12 +0300

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,1 +1,17 @@
+var l_status = 0;
+function g_load(type){
+  if(type == 1){
+    l_status = 1;
+    window.CrazyGames.SDK.game.sdkGameLoadingStart();
+  } else {
+    if(type == 0){
+      l_status = 0;
+      window.CrazyGames.SDK.game.sdkGameLoadingStop();
+    } else {
+      if(type == 'get'){
+        return l_status;
+      }
+    }
+  }
+}
 
