585dd74f2efbc12f06cd0ff071818caa4f81b873
Catcod24 <131913682+Catcod24@users.noreply.github.com>
2023-06-30 14:28:56 +0300

    Update ad.js

--- js/ad.js
+++ js/ad.js
@@ -1,1 +1,17 @@
-
+const ad_n = { 
+  adFinished: () => console.log("End midgame ad (callback)"), 
+  adError: (error) => console.log("Error midgame ad (callback)", error), 
+  adStarted: () => console.log("Start midgame ad (callback)"), 
+};
+const ad_r = { 
+  adFinished: () => console.log("End rewarded ad (callback)"), 
+  adError: (error) => console.log("Error rewarded ad (callback)", error), 
+  adStarted: () => console.log("Start rewarded ad (callback)"), 
+};
+function play_ad(type){
+  if(type == "m"){
+    window.CrazyGames.SDK.ad.requestAd("midgame", ad_n);
+  } else {
+    window.CrazyGames.SDK.ad.requestAd("rewarded", ad_r);
+  }
+}
