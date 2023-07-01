c2e65abf07e8e4ab4b5876616529e3bda0e32e4a
Catcod24 <131913682+Catcod24@users.noreply.github.com>
2023-07-01 14:05:43 +0300

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,6 +1,5 @@
-console.log('load.js imported');
-var l_status = 0;
-function g_load(type){
+export var l_status = 0;
+export function g_load(type){
   if(type == 1){
     l_status = 1;
     window.CrazyGames.SDK.game.sdkGameLoadingStart();
