7a0be81e613997dfa35d3d0b50309d616ff61439
Catcod24 <131913682+Catcod24@users.noreply.github.com>
2023-07-04 16:44:00 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,19 @@
 alert('x');
+var game = {
+  version_c: 0.1,
+  version_r: 0.1,
+  level: 0,
+  l_stage: 0,
+  l_total_stage: 0
+}
+var items = {
+  money: 0,
+  apple: 0
+}
+if (typeof(Storage) !== "undefined") {
+  if(localStorage.getItem('version_r') !== null){
+  } else {
+  }
+} else {
+  // Sorry! No Web Storage support..
+}
