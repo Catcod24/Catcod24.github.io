b4509192d4107dc66d7f14c14c476fef17a32ebc
Catcod24 <131913682+Catcod24@users.noreply.github.com>
2023-07-05 17:22:55 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ var game = {
   version_r: 0.1,
   level: 0,
   l_stage: 0,
-  l_total_stage: 0,
+  l_total_stage 0,
   ls: 9
 }
 var items = {
@@ -18,14 +18,36 @@ if (typeof(Storage) !== "undefined") {
   if(localStorage.getItem('version_r') !== null){
     var i = 0;
     while(i < gamek.lenght || i == gamek.lenght){
-      if(gamek[i] !== 'ls' || gamek[i] !== 'version_r'){
+      if(gamek[i] !== 'ls' || gamek[i] !== 'version_c'){
         i = i + 1;
         game[gamek[i]] = localStorage.getItem(gamek[i]);
       } else {
         i = i + 1;
       }
     }
-  } else {
+    i = 0;
+    while(i < itemsk.lenght || i == itemsk.lenght){
+      i = i + 1;
+      items[itemsk[i]] = localStorage.getItem(itemsk[i]);
+    }
+  } else {//aici setam elemente din game si setam local stoarge
+    var i = 0;
+    game.level = 1;
+    game.l_stage = 0;
+    game.l_total_stage = 1;
+    while(i < gamek.lenght || i == gamek.lenght){
+      if(gamek[i] !== 'ls' || gamek[i] !== 'version_c'){
+        i = i + 1;
+        game[gamek[i]] = localStorage.getItem(gamek[i]);
+      } else {
+        i = i + 1;
+      }
+    }
+    i = 0;
+    while(i < itemsk.lenght || i == itemsk.lenght){
+      i = i + 1;
+      items[itemsk[i]] = localStorage.getItem(itemsk[i]);
+    }
   }
 } else {
   game.ls = 0;
