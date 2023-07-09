fed4a5e63a66e57547cf71e2a93954a8f4fb8e31
Catcod24 <131913682+Catcod24@users.noreply.github.com>
2023-07-09 23:32:17 +0300

    Update index.js

--- 2/js/index.js
+++ 2/js/index.js
@@ -1,2 +1,8 @@
 var wr = document.createElement('canvas');
 wr.style.border = "1px";
+var dv = document.createElement('div');
+dv.style.textAlign = "center";
+vat btnp = document.createElement('button');
+btnp.style.background = "green";
+btnp.style.fontSize = "22px";
+btnp.style.fontFamily = "Times New Roman";
