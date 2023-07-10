4efd4c6aab972b3db95285996ea9f096abd43001
Catcod24 <131913682+Catcod24@users.noreply.github.com>
2023-07-10 22:08:28 +0300

    Create i.js

---
+++ 2/js/i.js
@@ -0,0 +1,7 @@
+var bup = document.createElement('button');
+bup.style.position = 'absolute';
+bup.style.left = '5%';
+bup.style.bottom = '15%';
+bup.style.width = '5%';
+bup.style.height = '5%';
+document.body.appendChild(bup);
