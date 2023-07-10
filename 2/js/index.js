1dfea531a93418cf6a8fadf9dbb44f6cc1efb80f
Catcod24 <131913682+Catcod24@users.noreply.github.com>
2023-07-10 10:59:54 +0300

    Update index.js

--- 2/js/index.js
+++ 2/js/index.js
@@ -1,5 +1,12 @@
-var wr = document.createElement('canvas');
-wr.style.border = "1px";
+var wri = document.createElement('button');
+wri.style.border = "0px";
+wri.style.backgroundColor = "white";
+wri.addEventListener("click", sel("i"));
+var wrl = document.createElement('button');
+wrl.style.border = "0px";
+wrl.style.backgroundColor = "Chartreuse";
+wrl.addEventListener("click", sel("l"));
+var select = "l";
 var dv = document.createElement('div');
 dv.style.textAlign = "center";
 var btnp = document.createElement('button');
@@ -9,3 +16,14 @@ btnp.style.fontFamily = "Times New Roman";
 btnp.innerHTML = "Play";
 document.body.appendChild(dv);
 dv.appendChild(btnp);
+function sel(w){
+  if(w == "i"){
+    select = "i";
+    wri.style.backgroundColor = "Chartreuse";
+    wrl.style.backgroundColor = "white";
+  } else {
+    select = "l";
+    wrl.style.backgroundColor = "Chartreuse";
+    wri.style.backgroundColor = "white";
+  }
+}
