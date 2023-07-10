c0a0cb482f387354467af26b1626d5da87b27c56
Catcod24 <131913682+Catcod24@users.noreply.github.com>
2023-07-10 11:41:49 +0300

    Update index.js

--- 2/js/index.js
+++ 2/js/index.js
@@ -1,11 +1,9 @@
 var wri = document.createElement('button');
 wri.style.border = "0px";
 wri.style.backgroundColor = "white";
-//wri.addEventListener("click", sel("i"));
 var wrl = document.createElement('button');
 wrl.style.border = "0px";
 wrl.style.backgroundColor = "Chartreuse";
-//wrl.addEventListener("click", sel("l"));
 var select = "l";
 var dv = document.createElement('div');
 dv.style.textAlign = "center";
@@ -16,6 +14,10 @@ btnp.style.fontFamily = "Times New Roman";
 btnp.innerHTML = "Play";
 document.body.appendChild(dv);
 dv.appendChild(btnp);
+dv.appendChild(wri);
+dv.appendChild(wrl);
+wri.addEventListener("click", sel("i"));
+wrl.addEventListener("click", sel("l"));
 function sel(w){
   if(w == "i"){
     select = "i";
