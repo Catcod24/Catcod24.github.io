ad3dae5adbc3812280061815b7c96e277c3e5c22
Catcod24 <131913682+Catcod24@users.noreply.github.com>
2023-07-10 11:30:09 +0300

    Update index.js

--- 2/js/index.js
+++ 2/js/index.js
@@ -1,11 +1,11 @@
 var wri = document.createElement('button');
 wri.style.border = "0px";
 wri.style.backgroundColor = "white";
-wri.addEventListener("click", sel("i"));
+//wri.addEventListener("click", sel("i"));
 var wrl = document.createElement('button');
 wrl.style.border = "0px";
 wrl.style.backgroundColor = "Chartreuse";
-wrl.addEventListener("click", sel("l"));
+//wrl.addEventListener("click", sel("l"));
 var select = "l";
 var dv = document.createElement('div');
 dv.style.textAlign = "center";
