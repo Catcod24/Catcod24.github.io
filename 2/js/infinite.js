ed96163f98a01e925e17973473662fe0357a55d2
Catcod24 <131913682+Catcod24@users.noreply.github.com>
2023-07-10 22:07:59 +0300

    Delete infinite.js

--- 2/js/infinite.js
+++
@@ -1,8 +0,0 @@
-var bup = document.createElement('button');
-bup.style.position = 'absolute';
-bup.style.left = '5%';
-bup.style.bottom = '15%';
-bup.style.width = '5%';
-bup.style.height = '5%';
-document.body.appendChild(bup);
-
